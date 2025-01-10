f97eefff6d75c54b89fdbdf570c12ad03f7674c6
Martin Holý <martin.holy.022@pslib.cz>
2025-01-10 08:51:04 +0100

    kvsklvs

--- GamebookTest1.Server/obj/Debug/net8.0/GamebookTest1.Server.AssemblyInfo.cs
+++ GamebookTest1.Server/obj/Debug/net8.0/GamebookTest1.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamebookTest1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb45b026a9da50ab67cdf7ef41acb6f8207c85e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f781139c5d2998dccbb3e29fb78e07c2af525613")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamebookTest1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamebookTest1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
