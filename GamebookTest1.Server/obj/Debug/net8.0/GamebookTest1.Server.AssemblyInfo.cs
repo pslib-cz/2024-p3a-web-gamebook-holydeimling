7f4479f378fc23347df147efe54628a361401647
JsemHarryDev <harry.deimling.022@pslib.cz>
2025-02-16 21:41:59 +0100

    database logic fixes + new data

--- GamebookTest1.Server/obj/Debug/net8.0/GamebookTest1.Server.AssemblyInfo.cs
+++ GamebookTest1.Server/obj/Debug/net8.0/GamebookTest1.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamebookTest1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7487ab054e8d0696b8457d69abe6412123ed0c75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22987710807f068338d2a5bcef0cb22586d1007d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamebookTest1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamebookTest1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
