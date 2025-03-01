e03c3a9412a783ec255c2309248f78058a7b1f47
JsemHarryDev <harry.deimling.022@pslib.cz>
2025-03-01 18:25:19 +0100

    kapitola 2👍

--- GamebookTest1.Server/obj/Debug/net8.0/GamebookTest1.Server.AssemblyInfo.cs
+++ GamebookTest1.Server/obj/Debug/net8.0/GamebookTest1.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamebookTest1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22987710807f068338d2a5bcef0cb22586d1007d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f4479f378fc23347df147efe54628a361401647")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamebookTest1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamebookTest1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
