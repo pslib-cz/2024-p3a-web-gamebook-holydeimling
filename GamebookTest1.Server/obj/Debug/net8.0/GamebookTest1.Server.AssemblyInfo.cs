63b1e3c416fb6b44fbc902aaefdf601ab99ce22a
JsemHarryDev <harry.deimling.022@pslib.cz>
2025-03-18 20:12:33 +0100

    mozna bude fungovat

--- GamebookTest1.Server/obj/Debug/net8.0/GamebookTest1.Server.AssemblyInfo.cs
+++ GamebookTest1.Server/obj/Debug/net8.0/GamebookTest1.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamebookTest1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3af87218a802ecf58e5603a613e11621c3cb13c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+718b0e3bb01fc4a03778383da467cb02e07f7d11")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamebookTest1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamebookTest1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
